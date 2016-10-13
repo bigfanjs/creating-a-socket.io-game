915c66c30976f1aabc298529ac4f149eba37634e
AdelJS <bigfanjs@gmail.com>
2016-10-14 00:47:49 +0100

    adding olaceholder code to server.js

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 import path from 'path';
+import express from 'express';
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.listen(3000, function () {
+  console.log('listening on port 3000');
+});
