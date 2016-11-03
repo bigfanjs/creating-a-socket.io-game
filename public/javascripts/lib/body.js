eb808c73967fb28c99ba59bf16f3a684d9aa637c
AdelJS <bigfanjs@gmail.com>
2016-11-03 23:36:46 +0000

    updating the body library component

--- public/javascripts/lib/body.js
+++ public/javascripts/lib/body.js
@@ -2,63 +2,63 @@ import vector from './vector';
 
 const
   create = Object.create,
-  assign = Object.assign,
-  behaviors = {
-    setup: function () {
-      const body = create( this );
+  assign = Object.assign;
 
-      Object.setPrototypeOf( body, vector );
-      assign( body, options );
+export default {
+  x: 0,
+  y: 0,
+  width: 50,
+  height: 50,
+  radius: 10,
+  color: '#888',
+  type: 'circle',
+  border: true,
+  setup: function ( options ) {
+    const proto = create( vector );
+    assign(proto, this);
 
-      return body;
-    },
-    draw: function ( ctx ) {
-      ctx.beginPath();
+    const body = create( proto );
+    assign( body, options );
 
-      if ( this.border ) {
-        ctx.strokeStyle = this.strokeStyle;
-        ctx.lineWidth = this.lineWidth;
-      }
-
-      ctx.fillStyle = this.color;
-
-      switch ( type ) {
-        case 'circle':
-          ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
-          break;
-        case 'body':
-          ctx.rect(this.x, this.y, this.width, this.height);
-          break;
-        case 'image':
-          ctx.drawImage(this.image, this.x, this.y, this.width. this.height);
-          break;
-      }
-
-      if (this.type !== 'image') { ctx.fill(); }
-      if (this.border) { ctx.stroke(); }
-    },
-    setPos: function ( x, y ) {
-      if (arguments.length !== 2) {
-        throw TypeError('setPos() accepts two arguments');
-      }
-
-      if (!Number( x ) || !Number( y )) {
-        throw TypeError('Invalid position');
-      }
-
-      this.y = y;
-      this.x = x;
-
-      return this;
-    }
+    return body;
   },
-  props = {
-    x: 0,
-    y: 0,
-    radius: 10,
-    color: '#888',
-    type: 'circle',
-    border: true
-  };
+  draw: function ( ctx ) {
+    ctx.beginPath();
 
-export default assign(create(behaviors), props);
+    if ( this.border ) {
+      ctx.strokeStyle = this.strokeStyle;
+      ctx.lineWidth = this.lineWidth;
+    }
+
+    ctx.fillStyle = this.color;
+
+    switch ( this.type ) {
+      case 'circle':
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+        break;
+      case 'body':
+        ctx.rect(this.x, this.y, this.width, this.height);
+        break;
+      case 'image':
+        ctx.drawImage(this.image, this.x, this.y/*, this.width, this.height*/);
+        break;
+    }
+
+    if (this.type !== 'image') { ctx.fill(); }
+    if (this.border) { ctx.stroke(); }
+  },
+  setPos: function ( x, y ) {
+    if (arguments.length !== 2) {
+      throw TypeError('setPos() accepts two arguments');
+    }
+
+    if (!Number( x ) || !Number( y )) {
+      throw TypeError('Invalid position');
+    }
+
+    this.y = y;
+    this.x = x;
+
+    return this;
+  }
+};
