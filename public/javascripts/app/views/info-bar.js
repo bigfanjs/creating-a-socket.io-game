e6b55272b6db91d32a61cfe3dd62372d609e22f0
AdelJS <bigfanjs@gmail.com>
2016-11-07 09:56:09 +0000

    creating information bar view

---
+++ public/javascripts/app/views/info-bar.js
@@ -0,0 +1,14 @@
+import Body from '../../lib/body';
+import Text from '../../lib/text';
+import Layout from '../../lib/layout';
+
+export default function ( options ) {
+  const layout = Layout.setup({
+    regions: {
+      clicks: {x: 20, y: 5},
+      timeLeft: {x: 15, y: 5},
+      score: {x: 10, y: 5},
+      player: {x: 5, y: 5}
+    }
+  });
+}
