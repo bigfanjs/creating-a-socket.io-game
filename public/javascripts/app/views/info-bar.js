396dcfec841acee5360639f916c2f78bd564ae51
AdelJS <bigfanjs@gmail.com>
2016-11-08 01:53:37 +0000

    fixing the information bar view

--- public/javascripts/app/views/info-bar.js
+++ public/javascripts/app/views/info-bar.js
@@ -1,14 +1,16 @@
 import Layout from '../../lib/layout';
 
 export default function ( options ) {
-  const layout = Layout.setup({
-    regions: {
-      clicks: {x: 20, y: 5},
-      timeLeft: {x: 15, y: 5},
-      score: {x: 10, y: 5},
-      player: {x: 5, y: 5}
-    }
-  });
+  const
+    layout = Layout.setup( options ),
+    canvas = layout.canvas;
+
+  layout.regions = {
+    clicks: {x: 20, y: 5},
+    timeLeft: {x: 15, y: 5},
+    score: {x: 10, y: 5},
+    player: {x: 5, y: 5}
+  };
 
   return layout;
 }
