86e3bfc297db61690519a3069bd8fb7bde26fa4e
AdelJS <bigfanjs@gmail.com>
2016-11-08 01:52:37 +0000

    creating the information bar layout's clicks view

---
+++ public/javascripts/app/views/clicks.js
@@ -0,0 +1,23 @@
+import Text from '../../lib/text';
+
+const assign = Object.assign;
+
+export default function ( options ) {
+  const { x, y } = options;
+
+  const text = Text.setup({
+    x, y,
+    text: 'Clicks',
+    fontSize: 16
+  });
+
+  const clicks = Text.setup({
+    x: x,
+    y: y + 10,
+    text: '5/10',
+    color: '#777777',
+    fontSize: 14
+  });
+
+  return clicks;
+}
