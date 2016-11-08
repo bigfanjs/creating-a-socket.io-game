7d8a1876b46cf6afb0ded603a7216221bc21cc7a
AdelJS <bigfanjs@gmail.com>
2016-11-08 01:09:43 +0000

    renaming layout view to game-layout view

---
+++ public/javascripts/app/views/game-layout.js
@@ -0,0 +1,21 @@
+import Layout from '../../lib/layout';
+
+export default function () {
+  const
+    layout = Layout.setup(),
+    canvas = layout.canvas;
+
+  layout.regions = {
+    picture: {
+      x: canvas.width/2,
+      y: 0,
+      width: 2,
+      height: canvas.height
+    },
+    infoBar: {width: canvas.width, height: 100},
+    players: {x: 500, y: 120},
+    centerBar: {x: canvas.width/2, y: 0}
+  };
+
+  return layout;
+}
