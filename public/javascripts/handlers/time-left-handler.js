fe61c878b335b5d2eb24cf76808c13ca0fab1f0a
AdelJS <bigfanjs@gmail.com>
2016-11-15 13:39:51 +0000

    fixing the client-side time left handler

--- public/javascripts/handlers/time-left-handler.js
+++ public/javascripts/handlers/time-left-handler.js
@@ -1,9 +1,9 @@
 import timeLeft from '../app/views/time-left';
 
-const time = timeLeft[ 0 ];
+const time = timeLeft()[ 1 ];
 
 export default function ( socket ) {
-  socket.on('time-left', function ( remainingtime ) {
-    time.text = remainingtime;
+  socket.on('time-left', function ( remainingTime ) {
+    time.text = remainingTime.text;
   });
 }
