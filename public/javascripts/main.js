973813289662d363290c4db85e0ecaf2b1c05548
AdelJS <bigfanjs@gmail.com>
2016-11-15 13:41:17 +0000

    fixing the app entry point [a wrong url]

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,7 +4,7 @@ import $ from 'jquery';
 import playersHandler from './handlers/players-handler';
 import loginResultHandler from './handlers/login-result-handler';
 import gameStartHandler from './handlers/game-start-handler';
-import timeLeftHandler from './handlers/time-left-hanlder';
+import timeLeftHandler from './handlers/time-left-handler';
 
 // import the login area template.
 import template from './templates/login-area.pug';
