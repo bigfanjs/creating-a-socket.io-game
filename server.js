35b94eda18185db18d2aa9f6dfc7e76edcb19b0a
AdelJS <bigfanjs@gmail.com>
2016-11-15 12:50:25 +0000

    updating the app placeholder server

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-import path from 'path';
-import express from 'express';
+'use strict';
 
-const app = express();
+const
+  http = require('http'),
+  path = require('path'),
+  express = require('express');
+
+const spotDiff = require('./lib/spot-diff');
+
+const
+  app = express(),
+  server = http.createServer( app );
 
 app.use(express.static(path.join(__dirname, './public')));
 
-app.listen(3000, function () {
+server.listen(3000, function () {
   console.log('listening on port 3000');
 });
+
+app.get('/api/picture', function ( req, res ) {
+
+});
+
+spotDiff( server );
