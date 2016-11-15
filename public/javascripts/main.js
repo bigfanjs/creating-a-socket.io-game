107af74801118de2a16d8eb81770d9a795ebf196
AdelJS <bigfanjs@gmail.com>
2016-11-15 12:44:01 +0000

    adding time left hanlder to the app entry point

---
+++ public/javascripts/main.js
@@ -0,0 +1,29 @@
+import $ from 'jquery';
+
+// import socket handlers.
+import playersHandler from './handlers/players-handler';
+import loginResultHandler from './handlers/login-result-handler';
+import gameStartHandler from './handlers/game-start-handler';
+import timeLeftHandler from './handlers/time-left-hanlder';
+
+// import the login area template.
+import template from './templates/login-area.pug';
+
+const socket = io.connect();
+
+window.onload = function () {
+  $('#container').html(template());
+
+  playersHandler( socket );
+  loginResultHandler( socket );
+  gameStartHandler( socket );
+  timeLeftHandler( socket );
+
+  $('#player-form').submit(function ( event ) {
+    event.preventDefault();
+    socket.emit('player', {
+      name: $('#name-input').val(),
+      playerNumbers: $('#players-num').val()
+    });
+  });
+};
