9573d3339c23d7f24cb1a6ca27178b6466078019
AdelJS <bigfanjs@gmail.com>
2016-11-16 10:52:30 +0000

    formating the the time

--- public/javascripts/handlers/time-left-handler.js
+++ public/javascripts/handlers/time-left-handler.js
@@ -1,9 +1,17 @@
-import timeLeft from '../app/views/time-left';
+import TimeLeft from '../app/views/time-left';
 
-const time = timeLeft()[ 1 ];
+const timeLeft = TimeLeft()[ 1 ];
 
 export default function ( socket ) {
-  socket.on('time-left', function ( remainingTime ) {
-    time.text = remainingTime.text;
+  socket.on('time-left', function ( time ) {
+    const
+      remainingTime = time.text,
+      seconds = Math.floor(remainingTime / 1000) % 60,
+      minutes = Math.floor(remainingTime / 1000 / 60) % 60,
+      s = (seconds < 10 ? '0' : '') + seconds,
+      m = (minutes < 10 ? '0' : '') + minutes,
+      text = s + ':' + m;
+
+    timeLeft.text = text;
   });
 }
