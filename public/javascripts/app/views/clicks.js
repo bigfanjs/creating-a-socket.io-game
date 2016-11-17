9b383654f008b0e2ffb84ef3b33ec4111d0cf6ce
AdelJS <bigfanjs@gmail.com>
2016-11-17 11:06:02 +0000

    turning the clicks view to a sinleton

--- public/javascripts/app/views/clicks.js
+++ public/javascripts/app/views/clicks.js
@@ -1,22 +1,25 @@
 import Text from '../../lib/text';
 
+export const text = Text.setup({
+  text: 'Clicks',
+  fontSize: 20
+});
+
+export const clicks = Text.setup({
+  text: '10',
+  color: 'red',
+  fontSize: 15,
+  textAlign: 'center'
+});
+
 export default function ( options ) {
   const { x, y } = options;
+  
+  text.x = x;
+  text.y = y;
 
-  const text = Text.setup({
-    x, y,
-    text: 'Clicks',
-    fontSize: 20
-  });
-
-  const clicks = Text.setup({
-    x: x + 25,
-    y: y + 20,
-    text: '5/10',
-    color: 'red',
-    fontSize: 15,
-    textAlign: 'center'
-  });
+  clicks.x = x + 25;
+  clicks.y = y + 20;
 
   return [ text, clicks ];
 }
