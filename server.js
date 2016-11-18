61df8aea3d1f5f2f13c9e6bf490fd33c98c294d1
AdelJS <bigfanjs@gmail.com>
2016-11-18 11:08:05 +0000

    updating server.js

--- server.js
+++ server.js
@@ -11,14 +11,27 @@ const
   app = express(),
   server = http.createServer( app );
 
+// routes:
+const
+  login = require('./routes/login'),
+  pictures = require('./routes/pictures');
+
+app.set('view engine', 'pug');
+
 app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/api/pictures', pictures.showPictures);
+app.get('/api/pictures/:id', pictures.viewPicture);
+app.post('/api/pictures', pictures.createPicture);
+app.put('/api/pictures/:id', pictures.updatePicture);
+app.delete('/api/pictures/:id', pictures.deletePicture);
+
+app.get('/admin/login', login.form);
+app.post('/admin/login', login.submit);
+app.get('/admin/logout', login.logout);
 
 server.listen(3000, function () {
   console.log('listening on port 3000');
-});
-
-app.get('/api/picture', function ( req, res ) {
-
 });
 
 spotDiff( server );
