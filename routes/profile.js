a40ff4293c8c761d8da5870911a6e4149bfb5ddf
AdelJS <bigfanjs@gmail.com>
2016-11-23 09:56:40 +0000

    adding the edit to the profile route

--- routes/profile.js
+++ routes/profile.js
@@ -1,13 +1,24 @@
 'use strict';
 
-const User = require('../models/user');
+const User = require('../models').User;
 
 exports.view = function (req, res, next) {
-  res.render('profile', { title: 'Profile' });
+  res.render('profile-view', { title: 'Profile' });
 };
 
 exports.form = function (req, res, next) {
   res.render('profile-edit');
+};
+
+exports.edit = function (req, res, next) {
+  User.update(
+    { _id: req.param._id },
+    req.body,
+    { multi: false },
+    (err, user) => {
+      if ( err ) return next( err );
+    }
+  );
 };
 
 exports.remove = function (req, res, next) {
