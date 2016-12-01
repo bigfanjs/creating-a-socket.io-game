dd68e9a12365498e88b03e734126ffd023daa818
AdelJS <bigfanjs@gmail.com>
2016-12-01 10:24:37 +0000

    adding more options to the user model

--- models/user.js
+++ models/user.js
@@ -3,7 +3,19 @@
 module.exports = function ( mongoose, db ) {
   const schema = new mongoose.Schema({
     name: String,
-    password: String
+    password: String,
+    email: String,
+    level: Number,
+    score: Number,
+    diffs: Number,
+    wins: Number,
+    looses: Number,
+    games: Number,
+    ranking: Number,
+    avatar: {
+      name: String,
+      path: String
+    }
   });
 
   return db.model('user', schema);
