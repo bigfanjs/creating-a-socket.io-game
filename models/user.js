4a5e4ddf98fc2ac8a462834d27f161a20c447892
AdelJS <bigfanjs@gmail.com>
2016-12-02 11:30:20 +0000

    adding more user-related props

--- models/user.js
+++ models/user.js
@@ -12,6 +12,14 @@ module.exports = function ( mongoose, db ) {
     looses: Number,
     games: Number,
     ranking: Number,
+    website: String,
+    birthday: String,
+    social: {
+      facebook: String,
+      twitter: String,
+      google: String,
+      github: String
+    },
     avatar: {
       name: String,
       path: String
