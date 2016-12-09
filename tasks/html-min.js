9695763715dbef8cf3a47f423e8d85adf683a107
AdelJS <bigfanjs@gmail.com>
2016-12-09 13:17:09 +0000

    replace the index.html with app.html in the html min gulp task

--- tasks/html-min.js
+++ tasks/html-min.js
@@ -3,7 +3,7 @@ import htmlMin from 'gulp-htmlmin';
 
 export default function () {
   return () => {
-    gulp.src('./public/index.html')
+    gulp.src('./public/app.html')
       .pipe(htmlMin({ collapseWhitespace: true }))
       .pipe(gulp.dest('../build/public'));
   };
