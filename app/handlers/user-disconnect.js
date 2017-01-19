90e2d525dc54502db20d20b7e59185b611cf5f7f
Adel Jojo <bigfanjs@gmail.com>
2017-01-19 17:39:31 +0000

    updating the user-disconnect handler

--- app/handlers/user-disconnect.js
+++ app/handlers/user-disconnect.js
@@ -1,31 +1,15 @@
-const
-  find = require('lodash/find'),
-  remove = require('lodash/remove');
+const remove = require('lodash/remove');
 
-module.exports = function (socket, io, groups) {
+module.exports = function (socket, io, queue) {
   socket.on('disconnect', function () {
-    const
-      name = socket.groupName,
-      player = socket.player,
-      group = find(groups, ['name', name]),
-      players = group ? group.players : [];
+    const name = socket.groupName;
 
-    if ( group ) {
-      if (players.length === 1) {
-        groups.splice(groups.indexOf( group ), 1);
-      } else {
-        remove(players, player => {
-          return player.id === socket.id;
-        });
-
-        io.to( group.name ).emit('players', {
-          amount: player.playerNumbers,
-          players: players.map(obj => obj.name),
-          avatar: player.avatar
-        });
-      }
-
-      socket.leave( group.name );
+    if (queue.includes(socket.player)) {
+      remove(queue, player => {
+        return player.socket.id === socket.id;
+      });
+    } else {
+      socket.leave( name );
     }
   });
 };
