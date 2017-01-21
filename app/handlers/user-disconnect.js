22e8fe6fe6f2b211aa099ecbe6eb7d4ba0f16726
Adel Jojo <bigfanjs@gmail.com>
2017-01-21 17:42:02 +0000

    updating the disconnect handler

--- app/handlers/user-disconnect.js
+++ app/handlers/user-disconnect.js
@@ -2,13 +2,15 @@ const remove = require('lodash/remove');
 
 module.exports = function (socket, io, queue) {
   socket.on('disconnect', function () {
-    const name = socket.groupName;
+    const
+      groups = Object.keys(io.sockets.adapter.rooms),
+      name = socket.groupName;
 
     if (queue.includes(socket.player)) {
       remove(queue, player => {
         return player.socket.id === socket.id;
       });
-    } else {
+    } else if (groups.includes(name)) {
       socket.leave( name );
     }
   });
