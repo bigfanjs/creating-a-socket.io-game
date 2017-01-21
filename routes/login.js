2181877723e8b89f0618eb75adb2699d6a47dd91
Adel Jojo <bigfanjs@gmail.com>
2017-01-21 17:46:58 +0000

    updating the user login handler

--- routes/login.js
+++ routes/login.js
@@ -14,7 +14,7 @@ exports.submit = function (req, res, next) {
 
     if ( user ) {
       req.session.uid = user._id;
-      res.redirect('/profile/view');
+      res.redirect('/profile');
     } else {
       req.flash('error', 'Sorry, invalid credantials!');
       res.redirect( req.failure );
