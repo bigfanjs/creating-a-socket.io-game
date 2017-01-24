9548def69bea4b0dab573f7bb8e5fcae5afd0a9b
Adel Jojo <bigfanjs@gmail.com>
2017-01-24 17:56:24 +0000

    updating the game layout

--- public/javascripts/app/views/game-layout.js
+++ public/javascripts/app/views/game-layout.js
@@ -1,26 +1,33 @@
 import Layout from '../../lib/layout';
 
-export default function (canvas, options) {
-  const layout = Layout.setup(canvas, options);
+export default function (canvas, drawings, options) {
+  const layout = Layout.setup(canvas, drawings, options);
+
+  const width = canvas.width - 100;
 
   layout.regions = {
     picture: {
       x: 0,
       y: 50,
-      width: canvas.width,
+      width: width,
       height: canvas.height-50
     },
     infoBar: {
       x: 0,
       y: 0,
-      width: canvas.width,
+      width: width,
       height: 100
     },
-    players: {x: 500, y: 120},
+    players: {
+      x: width+50,
+      y: 0,
+      width: width,
+      height: canvas.height
+    },
     centerBar: {
-      sx: canvas.width/2,
+      sx: width/2,
       sy: 50,
-      tx: canvas.width/2,
+      tx: width/2,
       ty: canvas.height
     }
   };
