dca9849f93227e1d506360e3d962340ab7b7f531
Adel Jojo <bigfanjs@gmail.com>
2017-01-24 17:49:23 +0000

    updating the client side click action

--- public/javascripts/app/actions/click.js
+++ public/javascripts/app/actions/click.js
@@ -2,7 +2,7 @@ import Body from '../../lib/body';
 
 const mouse = {x: 0, y: 0};
 
-export default function ( canvas, socket, actions ) {
+export default function (canvas, socket, actions) {
   canvas.addEventListener('click', function ( e ) {
     const
       circle = Body.setup({
@@ -21,6 +21,9 @@ export default function ( canvas, socket, actions ) {
 
     actions.push( circle );
 
-    socket.emit('click');
+    socket.emit('click', {
+      x: mouse.x,
+      y: mouse.y - 50
+    });
   }, false);
 }
